08546bc1f7ccfd19a8ce237073a3e9cbdfc6644d
Mj-gagne <mj.gagne@outlook.com>
2023-11-27 13:28:36 -0500

    Merge

--- include/DetectMicroSonore.h
+++ include/DetectMicroSonore.h
@@ -1,5 +1,5 @@
-#ifndef SONORE_H
-#define SONORE_H
+#ifndef DetectMicroSonore_H
+#define DetectMicroSonore_H
 #include <Arduino.h>
 #include <LibRobus.h>
 
@@ -9,14 +9,14 @@
 const int trigPin = 9;
 const int echoPin = 10;
 long duration;
-int distance;
+int distanceMur;
 
 void MicroSonoreSetup(){
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
-void MicroSonoreTest(){// Test
+void MicroSonoreTest(){
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
 
@@ -26,14 +26,16 @@ void MicroSonoreTest(){// Test
     digitalWrite(trigPin, LOW);
 
     duration = pulseIn(echoPin, HIGH, 1000000UL);
-    distance = duration * 0.034 / 2;
+    distanceMur = duration * 0.034 / 2;
 
     Serial.print("Distance: ");
-    Serial.println(distance);
+    Serial.println(distanceMur);
     
 }
 
-int MicroSonoreRobot(){//Fonction arret proche table
+int MicroSonoreRobot(){
+  //MOTOR_SetSpeed(RIGHT, 0.2);
+  //MOTOR_SetSpeed(LEFT, 0.2);
 
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -44,10 +46,47 @@ int MicroSonoreRobot(){//Fonction arret proche table
     digitalWrite(trigPin, LOW);
 
     duration = pulseIn(echoPin, HIGH, 1000000UL);
-    distance = duration * 0.034 / 2; // Convertion en Cm
+    distanceMur = duration * 0.034 / 2;
 
-    Serial.println("Distance :" + String(distance));
+    Serial.println("Distance :" + String(distanceMur));
 
-    return distance; 
+    return distanceMur;
 }
+
+//AVEC LCD
+/*
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(1,2,4,5,6,7);
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distanceCm, distanceInch;
+
+unsigned long previousMillis = 0;
+
+void setup(){
+    lcd.begin(16,2);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+void loop(){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distanceCm = duration * 0.034/2;
+    distanceInch = duration * 0.0133/2;
+    lcd. setCursor(0,0);
+    lcd.print("Distance:");
+    lcd.print(distanceCm);
+    delay(10);
+    lcd.print(" cm"); 
+    
+    lcd.setCursor(0,1);
+    lcd.print("Distance:");
+    lcd.print(distanceInch);
+    delay(10);
+    lcd.print(" inch");*/
     #endif
--- include/General.h
+++ include/General.h
@@ -3,9 +3,15 @@
 
 #include <LibRobus.h>
 #include <stdint.h>
+#define GAUCHE              1
+#define DROITE              0
 
 const uint8_t IR_DIST_MIN = 10;
 const uint8_t IR_DIST_MAX = 80;
+
+// Pour faire tourner le robot
+const int PULSES_TOURNER_90_DEG = 1940;
+const float VITESSE_TOURNER = 0.2;
 
 // Converti la valeur analogue d'un capteur infrarouge en cm
 // Le param "id" represente IR0 a IR3 (0-3)
@@ -13,10 +19,28 @@ float IR_to_cm(uint8_t id) {
   return constrain(5500.0/ROBUS_ReadIR(id), IR_DIST_MIN, IR_DIST_MAX);
 }
 
-// Fait avancer le robot avec l'ajustement PID et ajuste les coefficients de correction
-void avancer(float vitesseG, float vitesseD) {
+// Fait avancer le robot
+void avancer(float vitesseG, float vitesseD) 
+{
   MOTOR_SetSpeed(LEFT, vitesseG);
   MOTOR_SetSpeed(RIGHT, vitesseD);
+}
+
+void tourner90(int sens)
+{
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+
+  while (ENCODER_Read(0) < PULSES_TOURNER_90_DEG && ENCODER_Read(1) < PULSES_TOURNER_90_DEG)
+  {
+    (sens == DROITE) ? avancer(VITESSE_TOURNER, -1*VITESSE_TOURNER) : avancer(-1*VITESSE_TOURNER, VITESSE_TOURNER);
+    Serial.print(ENCODER_Read(0));
+    Serial.print("\t");
+    Serial.print(ENCODER_Read(1));
+    Serial.print("\n");
+  }
+  
+
 }
 
 
