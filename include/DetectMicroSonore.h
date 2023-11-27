737623a6e3f7543ab09f58d1ef09395cb2d5d1bb
Mj-gagne <mj.gagne@outlook.com>
2023-11-27 13:15:24 -0500

    Update DetectMicroSonore.h
    Rien pentoute

--- include/DetectMicroSonore.h
+++ include/DetectMicroSonore.h
@@ -50,41 +50,4 @@ int MicroSonoreRobot(){//Fonction arret proche table
 
     return distance; 
 }
-
-//AVEC LCD
-/*
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(1,2,4,5,6,7);
-const int trigPin = 9;
-const int echoPin = 10;
-long duration;
-int distanceCm, distanceInch;
-
-unsigned long previousMillis = 0;
-
-void setup(){
-    lcd.begin(16,2);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-}
-void loop(){
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distanceCm = duration * 0.034/2;
-    distanceInch = duration * 0.0133/2;
-    lcd. setCursor(0,0);
-    lcd.print("Distance:");
-    lcd.print(distanceCm);
-    delay(10);
-    lcd.print(" cm"); 
-    
-    lcd.setCursor(0,1);
-    lcd.print("Distance:");
-    lcd.print(distanceInch);
-    delay(10);
-    lcd.print(" inch");*/
     #endif
