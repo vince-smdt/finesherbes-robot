a370c61eaf51b35063fd9f8d386c87c0e62c192d
fredbouchard777 <frederique@propage.ca>
2023-11-27 09:25:28 -0500

    Update Bras.h

--- include/Bras.h
+++ include/Bras.h
@@ -1,13 +1,13 @@
 #ifndef BRAS_H
 #define BRAS_H
-
-#include <LibRobus.h>
+#include  <LibRobus.h>
+#include <stdint.h>
 #include <general.h>
 
 void lever_plateau();
 void deposer_plateau();
 
-void test_plateau()
+void test_plateau ()
 {
   lever_plateau();
   avancer (-0.2f, -0.2f);
@@ -27,22 +27,92 @@ void test_plateau()
 
 void lever_plateau()
 {
-  SERVO_Enable(0);
-  SERVO_SetAngle (0,120);
-  delay (1500);
-  SERVO_SetAngle (0,180);
-  delay (1500);
+SERVO_Enable(0);
+    SERVO_SetAngle (0,120);
+    delay (1500);
+    SERVO_SetAngle (0,180);
+    delay (1500);
 }
 
 void deposer_plateau()
 {
-  SERVO_Enable(0);
-  SERVO_SetAngle (0,120);
-  delay (1500);
+    SERVO_Enable(0);
+    SERVO_SetAngle (0,120);
+    delay (1500);
 }
 
-// Code à Imad, on pourrait utiliser les for loops dans sa fonction plateau pour gérer programmatiquement la vitesse des servos,
-// indépendamment de la résistante sur l'Arduino X
+void test_plateau1()
+{
+    SERVO_Enable(0);
+    SERVO_SetAngle (0,120);
+    delay (1500);
+    SERVO_SetAngle (0,180);
+    delay (1500);
+    SERVO_SetAngle (0,120);
+    delay (1500);
+    // SERVO_SetAngle(0, 180);
+    // Serial.print ("180");
+    // delay (3000);
+    // SERVO_SetAngle(0, 175);
+    // Serial.print ("175");
+    // delay (3000);
+    // SERVO_SetAngle(0, 170);
+    // Serial.print ("170");
+    // delay (3000);
+    // SERVO_SetAngle(0, 165);
+    // Serial.print ("165");
+    // delay (3000);
+    // SERVO_SetAngle(0, 160);
+    // Serial.print ("160");
+    // delay (3000);
+    // SERVO_SetAngle(0, 155);
+    // Serial.print ("155");
+    // SERVO_SetAngle(0, 150);
+    // Serial.print ("150");
+    // delay (3000);
+    // SERVO_SetAngle(0, 145);
+    // Serial.print ("145");
+    // delay (3000);
+    // SERVO_SetAngle(0, 140);
+    // Serial.print ("140");
+    // delay (3000);
+    // SERVO_SetAngle(0, 135);
+    // Serial.print ("135");
+    // delay (3000);
+    // SERVO_SetAngle(0, 130);
+    // Serial.print ("130");
+    // delay (3000);
+    // SERVO_SetAngle(0, 125);
+    // Serial.print ("125");
+    // delay (3000);
+    // SERVO_SetAngle(0, 120);
+    // Serial.print ("120");
+    // delay (3000);
+    // SERVO_SetAngle(0, 115);
+    // Serial.print ("115");
+    // delay (3000);
+    // SERVO_SetAngle(0, 110);
+    // Serial.print ("110");
+    // delay (3000);
+    // SERVO_SetAngle(0, 105);
+    // Serial.print ("105");
+    // delay (3000);
+    // SERVO_SetAngle(0, 100);
+    // Serial.print ("100");
+    // delay (3000);
+    // SERVO_SetAngle(0, 95);
+    // Serial.print ("95");
+    // delay (3000);
+    // SERVO_SetAngle(0, 90);
+    // Serial.print ("90");
+    // delay (3000);
+    // SERVO_SetAngle(0, 85);
+    // Serial.print ("85");
+    // delay (3000);
+    // SERVO_SetAngle(0, 80);
+    // Serial.print ("80");
+    // delay (3000);
+}
 
 // void plateau(){
 //   SERVO_Enable(1);
@@ -64,4 +134,16 @@ void deposer_plateau()
 //     }
 // }
 
-#endif // BRAS_H
+// plateau
+
+// #include "General.h"
+// // Detecter la balle et fait tomber le verre sur la balle
+// void pousserleverre(){
+//   Serial.print ("debut pousser le verre");
+//   SERVO_Enable (2);
+//   SERVO_SetAngle(1, 0);
+// }
+
+// #endif // BALLE_H
+
+#endif BRAS_H
