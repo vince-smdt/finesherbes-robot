22811d75d7e5a07680fc8fda4e6ad6db8d356513
JP Belval <jp@belval.org>
2023-11-30 12:15:03 -0500

    Update BLEFinesHerbes.h

--- include/BLEFinesHerbes.h
+++ include/BLEFinesHerbes.h
@@ -4,6 +4,7 @@
 #include <Arduino.h>
 #include <Queue.h>
 #include "Types.h"
+#include "Globales.h"
 #include <SoftwareSerial.h>
 
 const byte numChars = 4;
@@ -49,15 +50,11 @@ void recvData(){
 
 void printNewData(){
   if (newData == true) {
-
-    
     if(receivedChars[0] >= 1 && receivedChars[0] <= 6 && receivedChars[1] >= 1 && receivedChars[1] <= 6){
-      Commande nouvelle_Commande;
+      Commande* nouvelle_Commande = new Commande();
 
-      nouvelle_Commande.NumTable = receivedChars[0];
-      Serial.println(nouvelle_Commande.NumTable);
-      nouvelle_Commande.NumPlat = receivedChars[1];
-      Serial.println(nouvelle_Commande.NumPlat);
+      nouvelle_Commande->NumTable = receivedChars[0];
+      nouvelle_Commande->NumPlat = receivedChars[1];
       liste_Commandes.push(nouvelle_Commande);
     }
     newData = false;
